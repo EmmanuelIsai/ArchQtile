b38909acd3ca646b9a66018305c5a99fc5f00ddc
EmmanuelIsai <113325246+EmmanuelIsai@users.noreply.github.com>
2023-03-15 17:58:41 -0600

    Add files via upload

--- 1packages.sh
+++ 1packages.sh
@@ -14,6 +14,31 @@ mkdir Imagenes
 curl -O https://r4.wallpaperflare.com/wallpaper/625/89/275/archlinux-digital-art-linux-arch-linux-tech-hd-wallpaper-1f0cb881ced8a6f444c7620593386e99.jpg
 mv archlinux-digital-art-linux-arch-linux-tech-hd-wallpaper-1f0cb881ced8a6f444c7620593386e99.jpg Imagenes
 
+cd /home/*
+
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
+
+cd /home/*
+
+# Instala los extras necesarios de AUR y git para el funcionamiento del arc>
+
+sudo pacman -Syu
+yay -Syu
+
+# Instala qtile extras para agregar modificaciones visuales a qtile
+yay qtile-extras-git
+
+# Instala fuentes para los iconos de la barra "Nerd Font"
+yay ttf-nerd-fonts-symbols-2048-em-mono
+yay ttf-ubuntu-mono-nerd
+yay ttf-nerd-fonts-symbols-common
+
+cd /home/*
+
+# Instala temas para el menu de aplicaciones rofi
+git clone --depth=1 https://github.com/adi1090x/rofi.git
+cd rofi
+chmod +x setup.sh
+./setup.sh

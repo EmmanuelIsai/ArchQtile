32530a287f802ee3ea3ead451055f2479378e9d7
EmmanuelIsai <113325246+EmmanuelIsai@users.noreply.github.com>
2023-03-18 00:39:20 -0600

    Add files via upload

--- 1packages.sh
+++ 1packages.sh
@@ -3,44 +3,41 @@
 # Instala los paquetes necesarios para funcionamiento del archivo de
 # configuracion 
 
-cd /home/*
-
+cd $HOME
 sudo pacman -Syu
 sudo pacman -S firefox rofi which nitrogen pipewire ttf-dejavu
 sudo pacman -S ttf-liberation noto-fonts pavucontrol pamixer arandr
 sudo pacman -S ntfs-3g network-manager-applet xorg-xinit base-devel
 sudo pacman -S git pcmanfm glib2 gvfs lxappearance picom alacritty curl
 sudo pacman -S scrot python-psutil papirus-icon-theme gtk3 arc-gtk-theme
+sudo pacman -S lightdm ligthdm-gtk-greeter xorg-server-xephyr
 
 mkdir Imagenes
 curl -O https://r4.wallpaperflare.com/wallpaper/625/89/275/archlinux-digital-art-linux-arch-linux-tech-hd-wallpaper-1f0cb881ced8a6f444c7620593386e99.jpg
 mv archlinux-digital-art-linux-arch-linux-tech-hd-wallpaper-1f0cb881ced8a6f444c7620593386e99.jpg Imagenes
+curl -O https://images2.alphacoders.com/122/1224122.png
+sudo cp images /usr/share/pixmaps/ 
+mv images Imagenes
 
-cd /home/*
-
+cd $HOME
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
-cd /home/*
-
-# Instala los extras necesarios de AUR y git para el funcionamiento del arc>
-
+cd $HOME
+# Instala los extras necesarios de AUR y git para el funcionamiento del archivo config.py
 sudo pacman -Syu
 yay -Syu
-
 # Instala qtile extras para agregar modificaciones visuales a qtile
-yay qtile-extras-git
+yay qtile-extras-git ttf-nerd-fonts-symbols-2048-em-mono ttf-ubuntu-mono-nerd ttf-nerd-fonts-symbols-common
 
-# Instala fuentes para los iconos de la barra "Nerd Font"
-yay ttf-nerd-fonts-symbols-2048-em-mono
-yay ttf-ubuntu-mono-nerd
-yay ttf-nerd-fonts-symbols-common
-
-cd /home/*
-
+cd $HOME
 # Instala temas para el menu de aplicaciones rofi
 git clone --depth=1 https://github.com/adi1090x/rofi.git
 cd rofi
 chmod +x setup.sh
 ./setup.sh
+
+cd $HOME
+# Instala oh my bash
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"

1e8bc6f1d66dc1afd739fa6fa48afc34f7c3e7d4
EmmanuelIsai <113325246+EmmanuelIsai@users.noreply.github.com>
2023-03-18 16:50:45 -0600

    Update 1packages.sh

--- 1packages.sh
+++ 1packages.sh
@@ -10,7 +10,7 @@ sudo pacman -S ttf-liberation noto-fonts pavucontrol pamixer arandr
 sudo pacman -S ntfs-3g network-manager-applet xorg-xinit base-devel
 sudo pacman -S git pcmanfm glib2 gvfs lxappearance picom alacritty curl
 sudo pacman -S scrot python-psutil papirus-icon-theme gtk3 arc-gtk-theme
-sudo pacman -S lightdm lightdm-gtk-greeter xorg-server-xephyr
+sudo pacman -S lightdm lightdm-gtk-greeter xorg-server-xephyr brightnessctl
 
 mkdir Imagenes
 curl -O https://r4.wallpaperflare.com/wallpaper/625/89/275/archlinux-digital-art-linux-arch-linux-tech-hd-wallpaper-1f0cb881ced8a6f444c7620593386e99.jpg
